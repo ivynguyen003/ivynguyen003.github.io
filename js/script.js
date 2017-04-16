aea22c0d13e1afe0547169a6d0c1bb79f5e6bc39
ivynguyen003 <ivynguyen@Uyens-MacBook-Pro.local>
2017-04-15 21:21:56 -0700

    initial version

---
+++ js/script.js
@@ -0,0 +1,60 @@
+// external js: masonry.pkgd.js, imagesloaded.pkgd.js
+
+
+  // init Masonry
+  var $grid = $('.grid').masonry({
+    itemSelector: '.grid-item',
+    //percentPosition: true,
+    columnWidth: '.grid-sizer',
+    resize: true,
+    gutter: 10,
+  });
+
+  $grid.imagesLoaded().progress( function() {
+    $grid.masonry();
+  });  
+
+$(".modal-fullscreen").on('show.bs.modal', function () {
+  setTimeout( function() {
+    $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+  }, 0);
+});
+$(".modal-fullscreen").on('hidden.bs.modal', function () {
+  $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+});
+$(document).ready(function(){
+
+  $(".main").onepage_scroll({
+     sectionContainer: "section", // sectionContainer accepts any kind of selector in case you don't want to use section
+     easing: "ease", // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in", "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+     animationTime: 900, // AnimationTime let you define how long each section takes to animate
+     pagination: true, // You can either show or hide the pagination. Toggle true for show, false for hide.
+     updateURL: false // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+  });
+
+  
+});
+
+function init() {
+  
+  // start up after 2sec no matter what
+    window.setTimeout(function(){
+        start();
+    }, 2000);
+}
+
+// fade in experience
+function start() {
+  
+  $('body').removeClass("loading").addClass('loaded');
+  
+}
+
+$(window).load(function() {
+  
+  // fade in page
+  init();
+  
+});
+
+
